43e3331d691df04e2e3d3acda60c6ede1691b55e
ttokunag <ttokunag@ucsd.edu>
2020-02-08 05:24:40 +0000

    added comments in TrieNode.hpp

--- src/DictionaryTrie/TrieNode.hpp
+++ src/DictionaryTrie/TrieNode.hpp
@@ -1,3 +1,9 @@
+/**
+ * This is a header file for TrieNode class
+ *
+ * Author: Tomoya Tokunaga
+ */
+
 #ifndef TRIENODE_HPP
 #define TRIENODE_HPP
 
@@ -13,18 +19,25 @@ class TrieNode {
     TrieNode* middle;
     TrieNode* right;
 
+    /*
+     * a constructor of TrieNode class
+     */
     TrieNode(const char& letter) {
         data = letter;
         freq = 0;
         left = middle = right = nullptr;
     }
 
+    // set a data to a given character
     void setData(const char& d) { data = d; }
 
+    // set a freq to a given frequency
     void setFreq(const int& f) { freq = f; }
 
+    // get a data set to this node
     char getData() { return data; }
 
+    // get frequency set to this node
     int getFreq() { return freq; }
 };
 
